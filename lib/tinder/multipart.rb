804118900a02f0d671419d596c9cf35c47676662
Brandon Keepers <brandon@collectiveidea.com>
2009-01-28 21:34:54 -0500

    A little cleanup in the multipart classes

--- lib/tinder/multipart.rb
+++ lib/tinder/multipart.rb
@@ -1,4 +1,9 @@
-module Multipart
+require 'rubygems'
+require 'mime/types'
+require 'net/http'
+require 'cgi'
+
+module Multipart #:nodoc:
   # From: http://deftcode.com/code/flickr_upload/multipartpost.rb
   ## Helper class to prepare an HTTP POST request with a file upload
   ## Mostly taken from
@@ -8,40 +13,36 @@ module Multipart
   ##(bill@marginalia.org)
   ### NOW:
   ## Everything wrong is due to keith@oreilly.com
-  require 'rubygems'
-  require 'mime/types'
-  require 'net/http'
-  require 'cgi'
 
-  class Param
+  class Param #:nodoc:
     attr_accessor :k, :v
-    def initialize( k, v )
+    def initialize(k, v)
       @k = k
       @v = v
     end
 
     def to_multipart
-      #return "Content-Disposition: form-data; name=\"#{CGI::escape(k)}\"\r\n\r\n#{v}\r\n"
-      # Don't escape mine...
-      return "Content-Disposition: form-data; name=\"#{k}\"\r\n\r\n#{v}\r\n"
+      "Content-Disposition: form-data; name=\"#{k}\"\r\n\r\n#{v}\r\n"
     end
   end
 
-  class FileParam
+  class FileParam #:nodoc:
     attr_accessor :k, :filename, :content
-    def initialize( k, filename, content )
+    def initialize(k, filename, content)
       @k = k
       @filename = filename
       @content = content
     end
 
     def to_multipart
-      #return "Content-Disposition: form-data; name=\"#{CGI::escape(k)}\"; filename=\"#{filename}\"\r\n" + "Content-Transfer-Encoding: binary\r\n" + "Content-Type: #{MIME::Types.type_for(@filename)}\r\n\r\n" + content + "\r\n "
-      # Don't escape mine
-      return "Content-Disposition: form-data; name=\"#{k}\"; filename=\"#{filename}\"\r\n" + "Content-Transfer-Encoding: binary\r\n" + "Content-Type: #{MIME::Types.type_for(@filename)}\r\n\r\n" + @content + "\r\n"
+      "Content-Disposition: form-data; name=\"#{k}\"; filename=\"#{filename}\"\r\n" +
+        "Content-Transfer-Encoding: binary\r\n" +
+        "Content-Type: #{MIME::Types.type_for(@filename)}\r\n\r\n" +
+        @content + "\r\n"
     end
   end
-  class MultipartPost
+  
+  class MultipartPost #:nodoc:
     BOUNDARY = 'campfire-is-awesome'
     HEADER = {"Content-type" => "multipart/form-data, boundary=" + BOUNDARY + " "}
     TIMEOUT_SECONDS = 30
@@ -51,23 +52,13 @@ module Multipart
       @params = params
       @query = {}
       self.prepare_query
-      # puts "prepared query: #{query}"
     end
 
     def prepare_query()
-      # puts "entering prepare_query()"
-      fp = []
-      @params.each {|k,v|
-        if v.respond_to?(:read)
-          fp.push(FileParam.new(k, v.path, v.read))
-        else
-          fp.push(Param.new(k,v))
-        end
-      }
-      @query = fp.collect {|p| "--" + BOUNDARY + "\r\n" + p.to_multipart }.join("") + "--" + BOUNDARY + "--"
+      @query = @params.map do |k,v|
+        param = v.respond_to?(:read) ? FileParam.new(k, v.path, v.read) : Param.new(k, v)
+        "--#{BOUNDARY}\r\n#{param.to_multipart}"
+      end.join("") + "--#{BOUNDARY}--"
     end
-    
-  end 
-  
-  
+  end
 end
